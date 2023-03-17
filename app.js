1fb5dd3b0f54d0b604382a76054f35b5ec31d608
Hung Do Quoc <hung.do.quoc@student.tuke.sk>
2023-03-17 14:19:26 +0100

    scroll smooth

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const toggle = document.getElementById('toggle');
 const annually = [199.99,249.99,399.99];
 const monthy = [19.99,24.99,39.99];
 const price = document.querySelectorAll('.price :last-child');
+const links = document.querySelectorAll('a');
+
+links.forEach(elem => elem.addEventListener("click", smoothscroll));
+
+function smoothscroll(event){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
 
 toggle.addEventListener('click', () => {
     if(toggle.checked) {
